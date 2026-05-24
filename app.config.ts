0d489fbf18788959a1534d2493a5468b07ca8304
waimyolinn <thihablablabla@gmail.com>
2026-05-24 07:31:59 +0000

    Fix: Correct projectId and appSlug

--- app.config.ts
+++ app.config.ts
@@ -126,7 +126,7 @@ const config: ExpoConfig = {
   owner: "wiamyolinn",
   extra: {
     eas: {
-      projectId: "30451cbd-a868-4d74-8c68-85bcaf2cad3e"
+      projectId: "5a20b844-0045-412e-8948-1b208b84eac5"
     }
   }
 };
